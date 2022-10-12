4f5cfb20891aafb31babafa079f15dcd0b31f8ac
Alpcan Soydas <48163702+alpcansoydas@users.noreply.github.com>
2022-10-12 18:52:49 +0300

    Delete pgmreadpixels.py

--- pgmreadpixels.py
+++
@@ -1,6 +0,0 @@
-def mypgmread(x): #Prints first and secont pixel value
-    image = open(x,"r")
-    liste = image.readlines()
-    print(ord(liste[4][0]),ord(liste[4][1]))
-    image.close()
-mypgmread("test.pgm")
